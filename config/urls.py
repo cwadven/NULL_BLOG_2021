334194b66f98a09d7c91ad9318cdf67009a12d8d
이창우 <cwadven@naver.com>
2021-09-16 00:45:34 +0900

    feat: SEO robots.txt & sitemap 추가
    - robots 추가
    - sitemap 추가

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
+from django.http import HttpResponse
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+
+from board.models import Post
+
+info_dict = {
+    'queryset': Post.objects.filter(),
+    'date_field': 'created_at',
+}
 
 urlpatterns = [
     path('admin', admin.site.urls),
@@ -11,6 +21,12 @@ urlpatterns = [
     # social 용
     path('accounts/', include('allauth.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    # SEO 용
+    path('sitemap.xml', sitemap, {'sitemaps': {'board': GenericSitemap(info_dict, priority=0.6)}},
+         name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt/',
+         lambda x: HttpResponse("User-Agent: *\nDisallow: /admin/\nDisallow: /account/\nAllow: /",
+                                content_type="text/plain")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
