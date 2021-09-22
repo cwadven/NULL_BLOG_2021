7e5658502d1ab704a7d096776c16c1dd49a6c3ae
이창우 <cwadven@naver.com>
2021-09-22 20:52:52 +0900

    chore: 게시글에서 검색 가능
    - tag, title, body 검색 가능
    - urlencode 이용하여 query params 남겨놓기

--- board/views.py
+++ board/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponseRedirect
 
 from .models import *
 from control.models import *
-from django.db.models import Count
+from django.db.models import Count, Q
 
 from django.core.paginator import Paginator
+
 
 # 첫 화면
 def home(request):
@@ -15,8 +16,8 @@ def home(request):
 
     # 많이 보일 게시판의 글
     main_posts = Post.objects.annotate(
-        reply_count = Count('replys', distinct=True) + Count('rereply', distinct=True),
-        like_count = Count('likes', distinct=True),
+        reply_count=Count('replys', distinct=True) + Count('rereply', distinct=True),
+        like_count=Count('likes', distinct=True),
     ).order_by('-like_count', '-reply_count')[:6]
 
     all_tags = Tag.objects.all()
@@ -32,27 +33,33 @@ def home(request):
 
     return render(request, 'board/home.html', context)
 
+
 # 게시글 목록 (게시판)
 def board(request, board_url):
+    q = Q()
+
+    if request.GET.get('search'):
+        tag_id_list = Tag.objects.filter(tag_name__icontains=request.GET.get('search')).values_list('id', flat=True)
+        q = q & Q(title__icontains=request.GET.get('search')) | Q(body__icontains=request.GET.get('search')) | Q(tag_set__in=tag_id_list)
 
     board = get_object_or_404(Board, url=board_url)
-    posts = board.post_set.annotate(
-        reply_count = Count('replys', distinct=True) + Count('rereply', distinct=True),
-        like_count = Count('likes', distinct=True),
+    posts = board.post_set.filter(q).annotate(
+        reply_count=Count('replys', distinct=True) + Count('rereply', distinct=True),
+        like_count=Count('likes', distinct=True),
     ).order_by('-created_at')
 
-    #페이지네이션 만들기
+    # 페이지네이션 만들기
     posts = Paginator(posts, 10)
 
     page = request.GET.get('page')
 
-    #페이지 보이게 하는 숫자 구간
+    # 페이지 보이게 하는 숫자 구간
     page_numbers_range = 5
 
-    #최대 녀석이 있을 경우 최대 까지만 보이도록 하기 위해서!
+    # 최대 녀석이 있을 경우 최대 까지만 보이도록 하기 위해서!
     max_index = len(posts.page_range)
 
-    #페이지가 0일 경우 1로 변경 current_page에 넣기
+    # 페이지가 0일 경우 1로 변경 current_page에 넣기
     current_page = int(page) if page else 1
     start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
     end_index = start_index + page_numbers_range
@@ -62,21 +69,22 @@ def board(request, board_url):
 
     page_range = posts.page_range[start_index:end_index]
 
-    posts = posts.get_page(page) #페이지네이션 만들기
+    posts = posts.get_page(page)  # 페이지네이션 만들기
 
     context = {
-        "page_range":page_range,
-        'board':board,
-        'posts':posts,
+        "page_range": page_range,
+        'board': board,
+        'posts': posts,
     }
 
     return render(request, 'board/board.html', context)
 
+
 # 자세한 글 보기
 def post_detail(request, board_url, pk):
     qs = Post.objects.filter(board_id__url=board_url).annotate(
-        reply_count = Count('replys', distinct=True) + Count('rereply', distinct=True),
-        like_count = Count('likes', distinct=True),
+        reply_count=Count('replys', distinct=True) + Count('rereply', distinct=True),
+        like_count=Count('likes', distinct=True),
     ).order_by('-created_at')
 
     post = get_object_or_404(qs, board_id__url=board_url, pk=pk)
@@ -87,12 +95,13 @@ def post_detail(request, board_url, pk):
         like_check = False
 
     context = {
-        'like_check':like_check,
-        'qs':qs,
-        'post':post,
+        'like_check': like_check,
+        'qs': qs,
+        'post': post,
     }
 
     return render(request, 'board/post.html', context)
+
 
 # 댓글 작성
 def reply_write(request, board_url, pk):
@@ -102,6 +111,7 @@ def reply_write(request, board_url, pk):
             Reply.objects.create(post_id=post, author=request.user, body=request.POST.get('reply_body'))
 
     return HttpResponseRedirect(reverse('board:post', args=[board_url, pk]))
+
 
 # 답글 작성
 def rereply_write(request, board_url, pk):
@@ -114,6 +124,7 @@ def rereply_write(request, board_url, pk):
         rereply.save()
     return HttpResponseRedirect(reverse('board:post', args=[board_url, reply.post_id.id]))
 
+
 # 댓글 삭제
 def reply_delete(request, board_url, pk):
     if request.user.is_authenticated:
@@ -123,6 +134,7 @@ def reply_delete(request, board_url, pk):
             reply.delete()
     return HttpResponseRedirect(reverse('board:post', args=[board_url, post_id]))
 
+
 # 답글 삭제
 def rereply_delete(request, board_url, pk):
     if request.user.is_authenticated:
@@ -131,6 +143,7 @@ def rereply_delete(request, board_url, pk):
         if rereply.author == request.user or request.user.is_superuser:
             rereply.delete()
     return HttpResponseRedirect(reverse('board:post', args=[board_url, post_id]))
+
 
 # 좋아요 추가 삭제
 def like(request, board_url, pk):
@@ -143,25 +156,26 @@ def like(request, board_url, pk):
             Like.objects.create(author=request.user, post_id=post)
     return HttpResponseRedirect(reverse('board:post', args=[board_url, pk]))
 
+
 def tag_board(request, tag_name):
     tag_board = get_object_or_404(Tag, tag_name=tag_name)
     posts = tag_board.post_set.annotate(
-        reply_count = Count('replys', distinct=True) + Count('rereply', distinct=True),
-        like_count = Count('likes', distinct=True),
+        reply_count=Count('replys', distinct=True) + Count('rereply', distinct=True),
+        like_count=Count('likes', distinct=True),
     ).order_by('-created_at')
 
-    #페이지네이션 만들기
+    # 페이지네이션 만들기
     posts = Paginator(posts, 10)
 
     page = request.GET.get('page')
 
-    #페이지 보이게 하는 숫자 구간
+    # 페이지 보이게 하는 숫자 구간
     page_numbers_range = 5
 
-    #최대 녀석이 있을 경우 최대 까지만 보이도록 하기 위해서!
+    # 최대 녀석이 있을 경우 최대 까지만 보이도록 하기 위해서!
     max_index = len(posts.page_range)
 
-    #페이지가 0일 경우 1로 변경 current_page에 넣기
+    # 페이지가 0일 경우 1로 변경 current_page에 넣기
     current_page = int(page) if page else 1
     start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
     end_index = start_index + page_numbers_range
@@ -171,11 +185,11 @@ def tag_board(request, tag_name):
 
     page_range = posts.page_range[start_index:end_index]
 
-    posts = posts.get_page(page) #페이지네이션 만들기
+    posts = posts.get_page(page)  # 페이지네이션 만들기
 
     context = {
-        "page_range":page_range,
-        'tag_board':tag_board,
-        'posts':posts,
+        "page_range": page_range,
+        'tag_board': tag_board,
+        'posts': posts,
     }
     return render(request, 'board/board.html', context)
