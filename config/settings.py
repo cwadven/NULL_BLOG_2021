6033d4731dde748341219c36e04817e01f4f65cc
이창우 <cwadven@naver.com>
2022-03-02 00:07:43 +0900

    fix: crontab 수정중
    - crontab 오류 잡는 중

--- config/settings.py
+++ config/settings.py
@@ -180,7 +180,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CRONJOBS = [
-    ('0 0 * * *', 'config.cron.update_yesterday_and_today_visitor', '>> visitor_update.log')
+    ('* * * * *', 'config.cron.update_yesterday_and_today_visitor', '>> visitor_update.log')
 ]
 
 # Internationalization
