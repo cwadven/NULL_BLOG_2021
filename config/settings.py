3b387a9438b1e6f527682a4df7fde76988c48134
이창우 <cwadven@naver.com>
2022-03-01 22:16:02 +0900

    chore: visitor_middleware 명칭 수정
    - custom_middlewares

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'config.visitor_middleware.VisitorCountMiddleware',
+    'config.custom_middlewares.VisitorCountMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
