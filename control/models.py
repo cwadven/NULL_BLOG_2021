cc84a6ca4aa4746a84d849b90dd4fa3ebc4a43c9
이창우 <cwadven@naver.com>
2022-03-05 09:58:26 +0900

    feat: 방문자 판단 방식 변경
    - 계속 TodayYesterday 에서 데이터 쌓고 최신화 하는 경우 마지막 데이터 이용
    - 다음날 새로 생성할 때, 마지막 데이터 참조

--- control/models.py
+++ control/models.py
@@ -33,7 +33,7 @@ class IPVisitant(models.Model):
 class TodayYesterday(models.Model):
     today = models.PositiveIntegerField(default=0)
     yesterday = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return f'[{self.created_at}] Today: {self.today} / Yesterday: {self.yesterday}'
