fabf46af6824c9581f06474fbe06c42a14171529
이창우 <cwadven@naver.com>
2022-03-07 21:59:33 +0900

    chore: PEP8 규격 맞추기
    - 규격 맞추기

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,15 +2,17 @@ from django import forms
 from django.forms.widgets import ClearableFileInput
 from .models import *
 
+
 class MyClearableFileInput(ClearableFileInput):
     initial_text = '현재 사진'
     input_text = '사진 바꾸기'
     clear_checkbox_label = '지우기'
 
+
 class SignupForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'password', 'nickname', 'email', 'user_img', )
+        fields = ('username', 'password', 'nickname', 'email', 'user_img',)
         labels = {
             'username': '',
             'password': '',
@@ -24,34 +26,34 @@ class SignupForm(forms.ModelForm):
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'form-control',
-                'placeholder':'아이디(2글자이상)',
-                'style':'text-align:center',
-                'required':True,
-                'minlength':2,
+                'placeholder': '아이디(2글자이상)',
+                'style': 'text-align:center',
+                'required': True,
+                'minlength': 2,
             }),
             'password': forms.PasswordInput(attrs={
                 'class': 'form-control',
-                'placeholder':'비밀번호',
-                'style':'text-align:center',
-                'required':True,
-                'minlength':2,
+                'placeholder': '비밀번호',
+                'style': 'text-align:center',
+                'required': True,
+                'minlength': 2,
             }),
             'nickname': forms.TextInput(attrs={
                 'class': 'form-control',
-                'placeholder':'닉네임(2글자이상)',
-                'style':'text-align:center',
-                'required':True,
-                'minlength':2,
+                'placeholder': '닉네임(2글자이상)',
+                'style': 'text-align:center',
+                'required': True,
+                'minlength': 2,
             }),
             'email': forms.EmailInput(attrs={
                 'class': 'form-control',
-                'placeholder':'✉이메일',
-                'style':'text-align:center',
-                'required':True,
+                'placeholder': '✉이메일',
+                'style': 'text-align:center',
+                'required': True,
             }),
             'user_img': forms.FileInput(attrs={
-                'style':'',
-                'class':'form-control',
+                'style': '',
+                'class': 'form-control',
             })
         }
         error_messages = {
