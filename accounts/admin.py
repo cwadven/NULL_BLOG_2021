9e055bfc4c68cfd269ec2a1f6b00b5a7292a4ac0
이창우 <cwadven@naver.com>
2022-03-09 17:03:48 +0900

    feat: admin field 변경
    - admin field 변경

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email',
+        'nickname',
+        'first_name',
+        'last_name',
+        'date_joined',
+        'is_staff',
+        'is_active',
+        'last_login',
+    )
