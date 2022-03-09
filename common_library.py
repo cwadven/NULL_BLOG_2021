bb32bd553a65b4c51a1af7de0efa4baf507e0fd8
이창우 <cwadven@naver.com>
2022-03-09 20:51:19 +0900

    feat: paginator common 으로 분리
    - web paginator 기능 바깥으로 분리

--- common_library.py
+++ common_library.py
@@ -1,5 +1,7 @@
 import datetime
 
+from django.core.paginator import Paginator
+
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -14,3 +16,33 @@ def time_until_end_of_day(dt=None):
     if dt is None:
         dt = datetime.datetime.now()
     return ((24 - dt.hour - 1) * 60 * 60) + ((60 - dt.minute - 1) * 60) + (60 - dt.second)
+
+
+def web_paging(request, queryset, post_size=10, page_num_size=5):
+    """
+    request: http request
+    queryset: Model Queryset
+    post_size: 1 page post 개수
+    page_num_size: 페이지네이션 번호 보여질 개수 (1,2,3,4: 4개 / 1,2,3: 3개)
+    return: page_posts(paginator_queryset) / page_range(number_range)
+    """
+    page = request.GET.get('page', 1)
+    posts = Paginator(queryset, post_size)
+
+    max_index = len(posts.page_range)
+
+    current_page = int(page) if page else 1
+    start_index = int((current_page - 1) / page_num_size) * page_num_size
+    end_index = start_index + page_num_size
+
+    if end_index >= max_index:
+        end_index = max_index
+
+    page_range = posts.page_range[start_index:end_index]
+
+    page_posts = posts.get_page(page)  # 페이지네이션 만들기
+
+    return {
+        "page_posts": page_posts,
+        "page_range": page_range,
+    }
