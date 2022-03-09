86b2179e61abde51a6da79f7f1f6c424ae4932fe
이창우 <cwadven@naver.com>
2022-03-09 17:29:36 +0900

    refactor: PEP8 규격
    - PEP8 규격

--- board/context_processors.py
+++ board/context_processors.py
@@ -1,4 +1,8 @@
 from .models import Board, BoardGroup
 
+
 def nav_board(request):
-    return {'nav_board':Board.objects.filter(board_group=None), 'nav_group':BoardGroup.objects.order_by('-created_at')}
+    return {
+        'nav_board': Board.objects.filter(board_group=None),
+        'nav_group': BoardGroup.objects.order_by('-created_at')
+    }
