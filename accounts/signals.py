c025265b1216be5cc63f626846a5f32a6c972a7c
이창우 <cwadven@naver.com>
2022-03-13 13:00:18 +0900

    feat: logger 설정
    - 로거 설정

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,3 +1,4 @@
+import logging
 from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -5,6 +6,9 @@ from django.dispatch import receiver
 from allauth.socialaccount.models import SocialAccount
 
 from accounts.models import User, UserProvider
+
+
+logger = logging.getLogger('django')
 
 
 @receiver(post_save, sender=SocialAccount)
@@ -20,8 +24,8 @@ def receiver_social_signup(sender, instance, created, *args, **kwargs):
                 provider = UserProvider.objects.get(
                     name=social_info.provider,
                 )
-            except UserProvider.DoesNotExist:
-                pass
+            except UserProvider.DoesNotExist as e:
+                logger.error(e)
 
             if social_info.provider == 'naver':
                 user.provider = provider
