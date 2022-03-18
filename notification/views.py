6cc75f4f6d3947a9f44aaf0199d28976aff0e809
cwadven <cwadven@naver.com>
2022-03-18 15:50:27 +0900

    feat: notification return 데이터 가공
    - ajax 로 notification 요청할 경우 데이터 가공하여 내려주기

--- notification/views.py
+++ notification/views.py
@@ -2,34 +2,27 @@ import json
 
 from django.http import HttpResponse
 
-from accounts.models import User
 from notification.models import NotificationController
 
 
 def get_notification(request):
-    # if request.is_ajax():
     notification_infos = []
 
-    user = User.objects.get(user=request.user)
-
-    # if profile.recent != profile.alert:
-    #     profile.alert = profile.recent
-    #     profile.save(update_fields=['alert'])
-
-    notification_set = NotificationController.objects.select_related(
-        'replynotification__reply__post',
-        'rereplynotification__rereply__post',
-        'likenotification__like',
-        'notification_type',
-        'receiver',
-        'sender',
-    ).filter(
-        receiver=user,
-        is_checked=False,
-    ).order_by('-id')[:5]
+    if request.user.is_authenticated and request.user.has_notifications:
+        notification_set = NotificationController.objects.select_related(
+            'replynotification__reply__post',
+            'rereplynotification__rereply__post',
+            'likenotification__like',
+            'notification_type',
+            'receiver',
+            'sender',
+        ).filter(
+            receiver=request.user,
+            is_checked=False,
+        ).order_by('-id')
 
     for notification in notification_set:
         one_to_one_table = getattr(notification, notification.notification_type.name)
-        notification_infos.append(one_to_one_table.get_ajax_notice_response())
+        notification_infos.append(one_to_one_table.get_ajax_notification_response())
 
     return HttpResponse(json.dumps({'notification_infos': notification_infos}), 'application/json')
