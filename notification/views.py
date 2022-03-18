e403cf4f9a5b8c97d5dcd42b8a26dd56fb80af29
cwadven <cwadven@naver.com>
2022-03-18 15:53:50 +0900

    code: 코드 인덴트 수정
    - 코드 인덴트 수정

--- notification/views.py
+++ notification/views.py
@@ -21,8 +21,8 @@ def get_notification(request):
             is_checked=False,
         ).order_by('-id')
 
-    for notification in notification_set:
-        one_to_one_table = getattr(notification, notification.notification_type.name)
-        notification_infos.append(one_to_one_table.get_ajax_notification_response())
+        for notification in notification_set:
+            one_to_one_table = getattr(notification, notification.notification_type.name)
+            notification_infos.append(one_to_one_table.get_ajax_notification_response())
 
     return HttpResponse(json.dumps({'notification_infos': notification_infos}), 'application/json')
