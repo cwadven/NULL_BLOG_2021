07e782757152e16bc7c41f75530c499b7186a0e9
cwadven <cwadven@naver.com>
2022-03-18 16:37:51 +0900

    feat: 알림 체크하는 View 생성
    - 알림 체크하면 해당 게시글로 들어간다

--- notification/views.py
+++ notification/views.py
@@ -1,7 +1,10 @@
 import json
 
-from django.http import HttpResponse
+from django.db import transaction
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect
 
+from board.models import Reply, Rereply
 from notification.models import NotificationController
 
 
@@ -26,3 +29,50 @@ def get_notification(request):
             notification_infos.append(one_to_one_table.get_ajax_notification_response())
 
     return HttpResponse(json.dumps({'notification_infos': notification_infos}), 'application/json')
+
+
+def check_notification(request, notification_id):
+    if request.user.is_authenticated:
+        try:
+            notification_controller = NotificationController.objects.select_related(
+                'replynotification__reply__post__board',
+                'rereplynotification__rereply__post__board',
+                'likenotification__like__post__board',
+                'notification_type',
+                'receiver',
+                'sender',
+            ).get(
+                id=notification_id,
+                receiver=request.user,
+            )
+        except NotificationController.DoesNotExist as e:
+            raise Http404
+
+        with transaction.atomic():
+            notification_controller.is_checked = True
+            notification_controller.save(update_fields=["is_checked"])
+
+            if request.user.receiver_profile.filter(is_checked=False).count() == 0:
+                request.user.has_notifications = False
+                request.user.save(update_fields=["has_notifications"])
+
+        one_to_one_table = getattr(notification_controller, notification_controller.notification_type.name)
+
+        # reply, rereply, like 중 하나
+        lookup_model = getattr(one_to_one_table, notification_controller.notification_type.lookup_model)
+
+        # 위치 가져오기
+        post_id = lookup_model.post.id
+        board_name = lookup_model.post.board.name
+
+        # 최종 url 주소
+        final_url = f"/{board_name}/{post_id}"
+
+        # 만약 reply 면, 해당하는 댓글 # 로 보내기
+        # 만약 rereply 면, 해당하는 대댓글 # 로 보내기
+        if isinstance(lookup_model, Reply):
+            final_url += f"#reply_{lookup_model.id}"
+        elif isinstance(lookup_model, Rereply):
+            final_url += f"#rereply_{lookup_model.id}"
+
+        return redirect(f'{final_url}')
