f6d46d67afe0178dc95acfb271e3d4151e0277d9
cwadven <cwadven@naver.com>
2022-03-20 20:14:04 +0900

    feat: 효율성 증가
    - 만약 session storage 에 있을 경우 session storage 로, 없으면 request 요청 후 session storage 에 초기화

--- temp_static/base.js
+++ temp_static/base.js
@@ -1,16 +1,44 @@
-function get_board_set_from_board_group(board_group_id) {
-    $.ajax({
-        type: 'GET',
-        url: '/board-group/' + board_group_id + '/constant',
-        success: function (data) {
-            $('#from_board_group').css('display', 'flex');
-            $('#from_board_group').html('');
+function set_board_group_constant_at_session_storage(board_group_id, board_set) {
+    // session storage set
+    sessionStorage.setItem('board_group_' + board_group_id, JSON.stringify(board_set));
+}
 
-            data['board_set'].forEach(function (data){
-                $('#from_board_group').append(
-                    `<a style="word-break: keep-all;" class="nav-link" href="/${data.url}">${data.name}</a>`
-                );
-            });
-        }
+function get_board_group_constant_at_session_storage(board_group_id) {
+    // session storage search
+    let board_set;
+    try {
+        const board_set_json = sessionStorage.getItem('board_group_' + board_group_id);
+        board_set = JSON.parse(board_set_json);
+    } catch (e) {
+        board_set = null;
+    }
+    return board_set;
+}
+
+function display_board_group_constant(board_set) {
+    $('#from_board_group').css('display', 'flex');
+    $('#from_board_group').html('');
+
+    board_set.forEach(function (data) {
+        $('#from_board_group').append(
+            `<a style="word-break: keep-all;" class="nav-link" href="/${data.url}">${data.name}</a>`
+        );
     });
+}
+
+function get_board_set_from_board_group(board_group_id) {
+    const board_set = get_board_group_constant_at_session_storage(board_group_id);
+
+    if (board_set) {
+        display_board_group_constant(board_set);
+    } else {
+        $.ajax({
+            type: 'GET',
+            url: '/board-group/' + board_group_id + '/constant',
+            success: function (data) {
+                display_board_group_constant(data['board_set']);
+                set_board_group_constant_at_session_storage(board_group_id, data['board_set']);
+            }
+        });
+    }
 }
