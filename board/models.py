cd0762d9a1e46edc30a616abb52e1579b288e67f
cwadven <cwadven@naver.com>
2022-04-06 01:21:29 +0900

    feat: is_active 속성 추가
    - is_active 속성 추가로 활성화 비활성화 관리

--- board/models.py
+++ board/models.py
@@ -4,6 +4,8 @@ from ckeditor_uploader.fields import RichTextUploadingField
 import re
 from django.urls import reverse
 
+from board.managers import BoardManager
+
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
@@ -49,6 +51,9 @@ class Board(TimeStampedModel):
     board_img = models.ImageField(upload_to='board_img/', null=True, blank=True)
     board_group = models.ForeignKey(BoardGroup, on_delete=models.SET_NULL, null=True, blank=True)
     attribute = models.IntegerField(default=0, db_index=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = BoardManager()
 
     class Meta:
         verbose_name = "게시판"
