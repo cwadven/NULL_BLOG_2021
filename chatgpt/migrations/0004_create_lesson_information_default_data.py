52ac6bedceb9930d290a6fa23dc95aa63f8e97ae
cwadven <cwadven@naver.com>
2023-04-08 15:51:48 +0900

    feat: 마이그레이션
    - 마이그레이션

--- chatgpt/migrations/0004_create_lesson_information_default_data.py
+++ chatgpt/migrations/0004_create_lesson_information_default_data.py
@@ -4,6 +4,7 @@ from django.db import migrations
 def forward(apps, schema_editor):
     LessonInformation = apps.get_model('chatgpt', 'LessonInformation')
     LessonInformation.objects.get_or_create(
+        summary='Python Tip',
         system_prompt='너는 Python 시니어 개발자',
         prompt='이전까지 알려준 꿀팁 제외하고, 파이썬 3.7 이상 버전 꿀팁 코드를 최대 3개만 알려줘 예제 코드까지 작성해줘',
         tag='python',
@@ -13,6 +14,7 @@ def forward(apps, schema_editor):
 def backward(apps, schema_editor):
     LessonInformation = apps.get_model('chatgpt', 'LessonInformation')
     LessonInformation.objects.filter(
+        summary='Python Tip',
         system_prompt='너는 Python 시니어 개발자',
         prompt='이전까지 알려준 꿀팁 제외하고, 파이썬 3.7 이상 버전 꿀팁 코드를 최대 3개만 알려줘 예제 코드까지 작성해줘',
         tag='python',
